4e0ab82ddfd8aaa7dc66e9e2f5090474b8b61bae
Queendarlin <queendarlinnnamani@gmail.com>
2024-07-11 15:00:12 +0000

    Creating tables and adding constraint

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Create Users table with columns and constraints
+-- Adding enumartion for country also
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
