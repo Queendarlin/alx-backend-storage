9fea1c3eb274d27b6e439fe34aa6a98d1b2107d2
Queendarlin <queendarlinnnamani@gmail.com>
2024-07-16 12:53:02 -0700

    Improve 12-log_stats.py by adding the top 10 of the most present IPs in the collection nginx of the database logs

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,1 +1,56 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Module to print stats from a MongoDB collection named
+'nginx' in the 'logs' database.
+"""
+
+from pymongo import MongoClient
+
+
+def log_stats():
+    """
+    Prints log statistics from the MongoDB collection.
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    collection = db.nginx
+
+    # Total number of logs
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    # Number of logs for each method
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    # Number of logs with status check
+    status_check_count = collection.count_documents({"method": "GET",
+                                                     "path": "/status"})
+    print(f"{status_check_count} status check")
+
+    # Top 10 most present IPs
+    pipeline = [
+        {
+            "$group": {
+                "_id": "$ip",
+                "count": {"$sum": 1}
+            }
+        },
+        {
+            "$sort": {"count": -1}
+        },
+        {
+            "$limit": 10
+        }
+    ]
+    top_ips = collection.aggregate(pipeline)
+    print("IPs:")
+    for ip in top_ips:
+        print(f"\t{ip['_id']}: {ip['count']}")
+
+
+if __name__ == "__main__":
+    log_stats()
