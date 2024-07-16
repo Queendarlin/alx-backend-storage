bc0d2ebc336bb1308560173b5c4dc9a49dd351ed
Queendarlin <queendarlinnnamani@gmail.com>
2024-07-16 12:49:13 -0700

    Python function that returns all students sorted by average score

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,1 +1,30 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Module for interacting with MongoDB to find top students by average score.
+"""
+
+
+def top_students(mongo_collection):
+    """
+    Returns all students sorted by their average score.
+
+    Args:
+        mongo_collection. The collection object from pymongo.
+
+    Returns:
+        list: A list of students with their average scores,
+        sorted in descending order.
+    """
+    pipeline = [
+        {
+            "$project": {
+                "name": 1,
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {
+            "$sort": {"averageScore": -1}
+        }
+    ]
+
+    return list(mongo_collection.aggregate(pipeline))
