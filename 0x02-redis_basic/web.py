1c103b4d513cd3a22120ce3ee520fc281107aa8d
Queendarlin <queendarlinnnamani@gmail.com>
2024-07-17 20:24:06 +0000

    Storing lists

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,53 +1,38 @@
 #!/usr/bin/env python3
-"""Redis module"""
+'''A module with tools for request caching and tracking.
+'''
 import redis
 import requests
-from typing import Callable
 from functools import wraps
-
-# Initialize the Redis connection
-redis_client = redis.Redis()
+from typing import Callable
 
 
-def count_requests(method: Callable) -> Callable:
-    """
-    Decorator to count how many times a URL was accessed.
-    """
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
+
+
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
     @wraps(method)
-    def wrapper(url: str, *args, **kwargs) -> str:
-        # Increment the access count
-        redis_client.incr(f"count:{url}")
-        # Call the original method
-        return method(url, *args, **kwargs)
-    return wrapper
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
 
 
-def cache_result(expiration: int = 10) -> Callable:
-    """
-    Decorator to cache the result of the function with an expiration time.
-    """
-    def decorator(method: Callable) -> Callable:
-        @wraps(method)
-        def wrapper(url: str, *args, **kwargs) -> str:
-            # Check if the URL content is already cached
-            cached_result = redis_client.get(url)
-            if cached_result:
-                return cached_result.decode('utf-8')
-
-            # If not cached, call the original method to get the result
-            result = method(url, *args, **kwargs)
-            # Cache the result with an expiration time
-            redis_client.setex(url, expiration, result)
-            return result
-        return wrapper
-    return decorator
-
-
-@count_requests
-@cache_result(expiration=10)
+@data_cacher
 def get_page(url: str) -> str:
-    """
-    Get the HTML content of a particular URL and return it.
-    """
-    response = requests.get(url)
-    return response.text
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
