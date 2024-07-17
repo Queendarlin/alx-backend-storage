f302908e62d06aae081c232939a3b2d12edc0f80
Queendarlin <queendarlinnnamani@gmail.com>
2024-07-17 19:55:44 +0000

    Storing lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -63,6 +63,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store data in Redis with a random key and return the key.
